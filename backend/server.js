a36d63fd5c2770029e583040c49ce7c59a206481
Rushikesh pawar <rushikesh.pawar@nagarro.com>
2024-10-13 13:55:32 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { auth } = require("express-oauth2-jwt-bearer");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+app.use(cors({ origin: "http://localhost:3000" }));
+
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+  tokenSigningAlg: "RS256",
+});
+
+// enforce on all endpoints
+//app.use(jwtCheck);
+
+app.get("/api/protected", jwtCheck, (req, res) => {
+  res.json({ message: "This is a protected route." });
+});
+
+// Public Route
+app.get("/api/public", (req, res) => {
+  res.json({ message: "This is a public API route." });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Backend server running at http://localhost:${process.env.PORT}`);
+});
